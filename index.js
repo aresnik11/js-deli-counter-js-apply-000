3c4f5bf67e85b3385042b044b0827f530aaf94fb
Amy Resnik <aresnik11@gmail.com>
2019-06-13 01:12:01 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function takeANumber(line, name) {
+  return `Welcome, ${name}. You are number ${line.indexOf(name)+1} in line.`
+}
+
+function nowServing(line) {
+  if (line.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else {
+    return line.shift();
+  }
+}
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return "The line is currently empty.";
+  }
+  else {
+    var string = "The line is currently: ";
+    for (var i=0; i<line.length; i++) {
+      if (i+1 === line.length) {
+        string += `${i+1}. ${line[i]}`
+      }
+      else {
+        string += `${i+1}. ${line[i], }`
+      }
+    }
+    return string;
+  }
+}
